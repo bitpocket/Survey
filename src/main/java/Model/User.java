72811aaae6b4a2e801150e831be672a0306c8b57
Adrian Robak <adrianrobak@gmail.com>
2015-09-16 08:51:41 +0200

    Zapisywanie danych Usera

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author adrianrobak
+ */
+public class User {
+    public String Name;
+    public String Surname;
+    public int Id; 
+    public String Email;
+    public String Password;
+    public String Login;
+}
